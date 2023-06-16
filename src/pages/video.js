29c814cc9a33e80f9239e5721cce0e31550a11b9
alexiacgcosta <alexia.cassia09@gmail.com>
2023-06-16 18:09:36 -0400

    adding footer

--- src/pages/video.js
+++ src/pages/video.js
@@ -1,5 +1,6 @@
 import React, { useRef , useState } from 'react'
 import "./video.css"
+import VideoFooter from './components/footer/VideoFooter';
 
 function Video ()  {
   const videoRef = useRef(null);
@@ -27,6 +28,8 @@ function Video ()  {
       onClick={handdleStart}
       src="https://poqlymuephttfsljdabn.supabase.co/storage/v1/object/public/jornadadev/brecker2.mp4?t=2023-05-22T19%3A37%3A45.885Z"> 
       </video>
+      { /* sidebar */}
+      <VideoFooter/>
     </div>
   )
 }
